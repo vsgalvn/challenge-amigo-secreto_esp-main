871f44ea88d2723e154a816f134409ca240a2c3b
Vero <85841278+vsgalvn@users.noreply.github.com>
2025-02-07 01:16:46 -0300

    app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
 let amigos = [];
 
 function agregarAmigo(){
