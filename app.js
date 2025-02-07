b8fa593154b4996bb2c1b901d27bcff70d947f28
Vero <85841278+vsgalvn@users.noreply.github.com>
2025-02-07 00:56:20 -0300

    app.js

--- app.js
+++ app.js
@@ -1,1 +1,39 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo(){
+
+    const inputAmigo = document.getElementById("amigo");
+    const nombre = inputAmigo.ariaValueMax.trim();
+
+    if (nombreAmigo === ""){
+        alert("Inserte un nombre válido"); return;
+
+    }
+
+    amigos.push(nombreAmigo);
+
+
+    actualizarLista();
+
+
+    inputAmigo.value = "";
+
+    inputAmigo.focus();
+
+
+}
+
+function actualizarLista(){
+    const listaAmigoUl = document.getElementById("listaAmigos");
+
+    listaAmigoUl.innerHTML = ""; 
+
+    amigos.forEach(amigo => {const li = document.createElement("li");
+        li.textContent = amigo;
+        listaAmigoUl.appendChild("li");
+
+    });
+}
+
+
